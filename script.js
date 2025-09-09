3e0e5a5b6ff89036aaf1f87be2056d36d060dc3a
Tahmid Galib <galibtahmid@gmail.com>
2025-09-09 12:09:11 +0600

    data collection

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const categoryButtons=document.getElementsByClassName("category")
 
+function display(posts){
+    for(let post in posts){
+        console.log(posts[post]);
+    }
+}
+
 for(const btn of categoryButtons){
     btn.addEventListener('click', ()=>{
-        const url="https://openapi.programming-hero.com/api/plants";
-        fetch(url).then(res=>res.json()).then(data=>display(data)) 
+        const url="https://openapi.programming-hero.com/api/plants"
+fetch(url).then(res=>res.json())
+.then(data=>display(data))
     })
 }
 
 
 
-function display(posts){
-    posts.forEach(post => {
-        console.log(post);
-    });
-}
 
